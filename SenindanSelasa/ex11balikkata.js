1f75e886166f8f0c49f55e78d16e625bbd611c21
Mikel Gunawan <mikelgunawan95@gmail.com>
2019-03-12 15:57:44 +0700

    Revisi balik kata

--- SenindanSelasa/ex11balikkata.js
+++ SenindanSelasa/ex11balikkata.js
@@ -1,14 +1,23 @@
-function balikKata(kata) {
+/*function balikKata(kata) {
   // you can only write your code here!
   var panjangKata = kata.length;
   //return panjangKata;
   var temp = '';
-  for(panjangKata;panjangKata>=0;panjangKata--)
+*/
+function balikKata(kata){
+    var temp = '';
+    for (var i = kata.length - 1; i >= 0; i--) {
+        temp = temp + kata[i];
+    }
+    return temp;
+    
+}
+  /*for(panjangKata;panjangKata>=0;panjangKata--)
   {
    temp = temp + kata.charAt(panjangKata)
   }
   return temp;
-}
+}*/
 
 // TEST CASES
 console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
